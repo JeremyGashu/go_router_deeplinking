e3d36271ad328d214deb91962cb9826a1c85c092
“Ermias <gearmias@gmail.com>
2023-12-26 10:10:30 +0300

    tested deep linking in iOS

--- lib/main.dart
+++ lib/main.dart
@@ -34,10 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -46,11 +42,11 @@ class _MyHomePageState extends State<MyHomePage> {
               child: ElevatedButton(
                 onPressed: () {
                   context.pushNamed("news", pathParameters: {
-                    "id": "jeremygashu",
+                    "id": "dwirandyh",
                     "path": "flutter-deeplink-example"
                   });
                 },
-                child: const Text("Go to News Page"),
+                child: const Text("Go to News"),
               ),
             )
           ],
